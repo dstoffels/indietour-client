5db66b9dc5aeecec45ec3a9831b261f219c7f763
dstoffels <dan.stoffels@gmail.com>
2023-07-12 22:57:34 -0500

    feat: middleware refresh flow

--- src/utils/apiRoutes.ts
+++ src/utils/apiRoutes.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const apiRoutes = axios.create({
 	baseURL: '/api',
+	withCredentials: false,
+	headers: {
+		'Content-Type': 'application/json',
+	},
 });
 
 export default apiRoutes;
