1dabf46fe6b627248d0fa0a3c85bd7ae1dbfc242
Danimal <dan.stoffels@gmail.com>
2024-01-14 02:17:22 -0600

    fix: linting

--- src/components/DATES/DateDrawer/MobileDrawer.tsx
+++ src/components/DATES/DateDrawer/MobileDrawer.tsx
@@ -5,23 +5,15 @@ import { NewDateFormProps } from '../NewDateForm/NewDateForm';
 import { useGlobals } from 'context/GlobalContext';
 
 const MobileDrawer = (props: NewDateFormProps) => {
-	const { dateDrawerOpen, toggleDateDrawer: setDateDrawerOpen } = useGlobals();
-
-	const handleOpen = () => {
-		!dateDrawerOpen && setDateDrawerOpen(true);
-	};
-
-	const handleClose = () => {
-		dateDrawerOpen && setDateDrawerOpen(false);
-	};
+	const { dateDrawerOpen, toggleDateDrawer } = useGlobals();
 
 	return (
 		<SwipeableDrawer
 			swipeAreaWidth={30}
 			anchor="left"
 			open={dateDrawerOpen}
-			onOpen={handleOpen}
-			onClose={handleClose}
+			onOpen={toggleDateDrawer}
+			onClose={toggleDateDrawer}
 			disableSwipeToOpen={false}
 		>
 			<DrawerContent {...props} />
--- src/components/DATES/SCHEDULE/ScheduleDrawer/MobileDrawer.tsx
+++ src/components/DATES/SCHEDULE/ScheduleDrawer/MobileDrawer.tsx
@@ -4,22 +4,15 @@ import { PropsWithChildren } from 'react';
 import DrawerContents from './DrawerContents';
 
 const MobileDrawer = () => {
-	const { scheduleDrawerOpen, toggleScheduleDrawer: setScheduleDrawerOpen } = useGlobals();
-
-	const handleOpen = () => {
-		setScheduleDrawerOpen(true);
-	};
-	const handleClose = () => {
-		setScheduleDrawerOpen(false);
-	};
+	const { scheduleDrawerOpen, toggleScheduleDrawer } = useGlobals();
 
 	return (
 		<SwipeableDrawer
 			swipeAreaWidth={30}
 			anchor="right"
 			open={scheduleDrawerOpen}
-			onOpen={handleOpen}
-			onClose={handleClose}
+			onOpen={toggleScheduleDrawer}
+			onClose={toggleScheduleDrawer}
 			disableSwipeToOpen={false}
 		>
 			<DrawerContents />
